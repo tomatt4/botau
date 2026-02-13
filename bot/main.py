3562244f4fa84609f45815cf3d25714ce53a1e5c
osalvaontop <santosmiguelalves6@gmail.com>
2026-02-13 19:20:34 -0300

    Load welcome cog in main.py

--- bot/main.py
+++ bot/main.py
@@ -36,6 +36,7 @@ class RizeBot(commands.Bot):
         await self.load_extension("cogs.mensagem")
         await self.load_extension("cogs.embeds")
         await self.load_extension("cogs.word_filter")
+        await self.load_extension("cogs.welcome")
 
         print("Lumi: Cogs carregados")
 
