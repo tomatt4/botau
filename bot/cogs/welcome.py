df042b758200a7dd9ec86de2040749e3e7117a1a
osalvaontop <santosmiguelalves6@gmail.com>
2026-02-13 19:20:04 -0300

    Add Welcome cog to greet new members

---
+++ bot/cogs/welcome.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+class Welcome(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        CHANNEL_ID = 1454971586758180907  # coloca o ID do canal aqui
+
+        channel = member.guild.get_channel(CHANNEL_ID)
+        if not channel:
+            return
+
+        await channel.send(f"{member.mention} entrou no servidor! Recepção notificada automaticamente.\n\n-# ||<@&1420778000504324238>||")
+
+async def setup(bot):
+    await bot.add_cog(Welcome(bot))
