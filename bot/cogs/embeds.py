ede99258512280b5d5e55e7117dc4ca4e27b736a
osalvaontop <santosmiguelalves6@gmail.com>
2026-02-13 21:02:27 -0300

    Refactor cores_embed method in embeds.py
    Removed detailed description from cores_embed method and adjusted embed color.

--- bot/cogs/embeds.py
+++ bot/cogs/embeds.py
@@ -10,18 +10,11 @@ class Embeds(commands.Cog):
         embed = discord.Embed(
             title="",
             description=(
-                "# *<@&1469484491767550184>*\n\n"
-                "para aqueles que apoiam a comunidade de maneira simples, mas significativa.\n\n"
-                "o cargo **Exctinct Archangels** é concedido para quem coloca o link do servidor em sua bio — um gesto discreto, mas poderoso, que mostra que você faz parte da essência do nosso servidor.\n\n"
-                "**permissões básicas:**\n"
-                "• fotos, e vídeos no chat.\n"
-                "• reconhecimento especial entre os membros.\n"
-                "• direitos básicos de interação no servidor.\n\n"
-                "mesmo com poucas permissões, ser **extinct archangels** é um símbolo de pertencimento e apoio à comunidade. Você já faz parte do grupo que mantém o servidor vivo."
+                ""
             
             ),
 
-            color=0xFFFFFF
+            color=0xFFFFF
         )
         await ctx.send(embed=embed)
         embed.set_footer(text="")
