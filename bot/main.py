27d27426bd6f27dfb4c6411b3dd915fd39e4a224
osalvaontop <santosmiguelalves6@gmail.com>
2026-03-10 20:47:10 -0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -8,7 +8,7 @@ import threading
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=",", intents=intents)
 
 # =========================
 # SERVIDOR FLASK
