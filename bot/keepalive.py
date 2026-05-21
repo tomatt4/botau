c1eee176507a5cb76b81b235730a30663e37013b
osalvaontop <santosmiguelalves6@gmail.com>
2026-05-21 14:36:33 -0300

    Use environment variable for server port
    Update the run function to use environment variable for port.

--- bot/keepalive.py
+++ bot/keepalive.py
@@ -8,7 +8,7 @@ def home():
     return "Bot online!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 def keep_alive():
     t = Thread(target=run)
