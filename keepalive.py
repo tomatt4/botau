7121aab5fa68302de4e53bcb330d3a0c1be55668
osalvaontop <santosmiguelalves6@gmail.com>
2026-06-16 02:40:48 -0300

    Change app port to 5000 in keepalive.py

--- keepalive.py
+++ keepalive.py
@@ -8,8 +8,7 @@ def home():
     return "Bot online!"
 
 def run():
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-
+    app.run(host="0.0.0.0", port=5000)
 def keep_alive():
     t = Thread(target=run)
     t.start()
